0fc297f4330f97429989a7b60069216a2c56fe34
Thiago Alexandre Martins Monteiro <thicmp@gmail.com>
2018-09-26 23:41:54 -0300

    Criada lista de arquivos a serem ignorados pelo Git.

---
+++ syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# 1. Indentations
+if 5 > 2:
+    print("Five is greater than two!")
+
+# 2. Comments
+# This is a comment.
+print("Hello, World!")
+
+# 3. Docstrings
+""" Thi sis a
+multiline docstring."""
+print("Hello, World!")
+

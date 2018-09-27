dd832e5a00580804223a03b5c86735a2ab36dc12
Thiago Alexandre Martins Monteiro <thicmp@gmail.com>
2018-09-27 00:10:56 -0300

    Adicionado exemplos com variáveis.

---
+++ variables.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Python Variables
+
+# Creating Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+# Variable Names
+#
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores
+# Variable names are case-sensitive
+
+# Ouput Variables
+x = "awesome"
+
+print("Python is " + x)
+
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x + y)

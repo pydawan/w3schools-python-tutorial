52576626757cae5c5d9bda092d5859c8bd775a3d
Thiago Alexandre <thicmp@gmail.com>
2018-10-01 17:01:43 -0300

    Adicionado exemplo inicial de programação orientada a objetos em Python.

---
+++ classes-objects.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Python Classes and Objects
+
+# Python is an object oriented programming language.
+# Almost everything in Python is an object, with its properties and methods.
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+
+# To create a class, use the keyword class:
+class MyClass:
+    x = 5
+
